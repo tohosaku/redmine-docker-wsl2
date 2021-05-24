3400f217655556488b9873f03dcb5f63d24f3ab7
tohosaku <tohosaku@osdn.me>
2021-05-25 06:47:07 +0900

    Initial Commit

---
+++ dc_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+USER_ID=`id | sed -e 's/^uid=\(.*\)(\(.*\)) gid=.*$/\1/'`
+GROUP_ID=`id | sed -e 's/uid=.* gid=\(.*\)(\(.*\)) groups=.*/\1/'`
+
+LOCAL_UID=${USER_ID} LOCAL_GID=${GROUP_ID} docker-compose -f docker-compose.yml -f docker-compose-test.yml $*
