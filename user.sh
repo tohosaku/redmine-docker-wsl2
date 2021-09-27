a5c802a443e218c5119e19a7448676bf857caaf7
tohosaku <tohosaku@osdn.me>
2021-09-27 04:08:50 +0000

    Support Remote Container

--- user.sh
+++ user.sh
@@ -1,2 +1,2 @@
-USER_ID=`id | sed -e 's/^uid=\(.*\)(\(.*\)) gid=.*$/\1/'`
-GROUP_ID=`id | sed -e 's/uid=.* gid=\(.*\)(\(.*\)) groups=.*/\1/'`
+echo LOCAL_UID=`id | sed -e 's/^uid=\(.*\)(\(.*\)) gid=.*$/\1/'` > .user.env
+echo LOCAL_GID=`id | sed -e 's/uid=.* gid=\(.*\)(\(.*\)) groups=.*/\1/'` >> .user.env
